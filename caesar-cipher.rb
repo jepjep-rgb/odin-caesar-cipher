c8d8ef284e49bf6caa8b0ca71f11f70d42633d3e
jepjep-rgb <jefroyalde@gmail.com>
2022-08-10 16:33:35 +0800

    Implement function to simplify the caesar shift

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,11 @@
+def shift_check(shift)
+  while shift < 0
+    shift = 26 + shift
+  end
+  shift = shift % 26
+end
+
 def caesar_cipher(string, shift)
   char_numbers = string.split('').map { |char| char.ord }
+  norm_shift = shift_check(shift)
 end
