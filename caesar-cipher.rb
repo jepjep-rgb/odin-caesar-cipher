e9832328f946580a0985cb052a8e947225d00b62
jepjep-rgb <jefroyalde@gmail.com>
2022-08-10 18:43:36 +0800

    Implement function to check for string characters
    The function checks whether the every character in the string input
    is a letter of the alphabet.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,12 @@
+def caesar_check(char_numbers)
+  char_numbers.each do |char|
+    unless char.between?(65, 122)
+      return false
+    end
+  end
+  return true
+end
+
 def shift_check(shift)
   while shift < 0
     shift = 26 + shift
@@ -8,4 +17,8 @@ end
 def caesar_cipher(string, shift)
   char_numbers = string.split('').map { |char| char.ord }
   norm_shift = shift_check(shift)
+
+  unless caesar_check(char_numbers)
+    return
+  end
 end
